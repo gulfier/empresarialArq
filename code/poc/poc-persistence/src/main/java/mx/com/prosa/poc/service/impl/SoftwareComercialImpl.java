03c9e457a94a6a01617e1e75ad975ca46ccc6ef3
Jorge <jorge.ortegah@axity.com>
2021-01-22 15:50:20 -0600

    SoftwareComercial delete and update methods

--- code/poc/poc-persistence/src/main/java/mx/com/prosa/poc/service/impl/SoftwareComercialImpl.java
+++ code/poc/poc-persistence/src/main/java/mx/com/prosa/poc/service/impl/SoftwareComercialImpl.java
@@ -9,6 +9,7 @@ import mx.com.prosa.poc.persistence.TblMarcasRepository;
 import mx.com.prosa.poc.persistence.TblSoftwareComercialRepository;
 import mx.com.prosa.poc.service.SoftwareComercialService;
 import mx.com.prosa.poc.to.SoftwareComercialTO;
+import mx.com.prosa.poc.util.BaseTOValidationUtil;
 import mx.com.prosa.poc.util.SupplierBusinessException;
 
 // TODO: Auto-generated Javadoc
@@ -46,4 +47,42 @@ public class SoftwareComercialImpl implements SoftwareComercialService {
 		tblSoftwareComercial.save(entity);
 	}
 
+	/**
+	 * Delete.
+	 *
+	 * @param id the id
+	 * @return the boolean
+	 */
+	@Override
+	public Boolean delete(Long id) {
+		tblSoftwareComercial.delete(tblSoftwareComercial.
+				findById(id).orElseThrow(SupplierBusinessException.SOFTWARE_NOT_FOUND));
+		return true;
+	}
+	
+	/**
+	 * Edits the.
+	 *
+	 * @param software the software
+	 * @return the boolean
+	 */
+	@Override
+	public Boolean edit(SoftwareComercialTO software) {
+		BaseTOValidationUtil.validateIdNotNull( software );
+		TblSoftwareComercial entity = this.tblSoftwareComercial.findById( software.getId() )
+	        .orElseThrow( SupplierBusinessException.IT_SERVICE_NOT_FOUND );
+		entity.setDsNombre(software.getDsNombre());
+		entity.setDsTipo(software.getDsTipo());
+		entity.setDsVersion(software.getDsVersion());
+		
+		if(software.getFkIdMarca()!=null) {
+			TblMarcas marca = tblMarcasRepository.findById(software.getFkIdMarca())
+						.orElseThrow(SupplierBusinessException.BRAND_NOT_FOUND);
+			entity.setFkIdMarca(marca);
+		}
+
+	    this.tblSoftwareComercial.save( entity );
+	    this.tblSoftwareComercial.flush();
+		return true;
+	}
 }
