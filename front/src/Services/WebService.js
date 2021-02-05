516775c42a9430ae981e4463c8f1fa3201f13921
Jorge Ortega Hernandez <jorge.ortegah@axity.com>
2021-02-04 19:42:25 -0600

    Changes in deployment

--- front/src/Services/WebService.js
+++ front/src/Services/WebService.js
@@ -3,7 +3,7 @@ import {http} from '../Util/scripts/http.service';
 const TOKEN = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJndWlsbGVybW8uc2VndXJhQGF4aXR5LmNvbSIsImF1ZCI6IkludGVyZmF6Ok1lZ2EvUmVtZWR5Iiwic2NwIjoiYXBpL3BlcnNpc3RlbmNlIiwiaXNzIjoiaHR0cHM6Ly93d3cucHJvc2FtZXhpY28ubXgvIiwiZXhwIjo2MTYwNzkzNjI2MSwiaWF0IjoxNjA3OTM2MzIxfQ.J_gAk5pwbXjpm6aLF-mBIes66vKvpLIBWMZsoFxo6v4';
 
 // const urlBase = 'http://localhost:';
-const urlBase = "http://web-prosa.192.168.99.103.nip.io";
+const urlBase = "http://web-prosa.192.168.99.103.nip.io:";
 const portPersistence = '8080';
 const portWeb = '8080';
 const baseURL = '/v1';
