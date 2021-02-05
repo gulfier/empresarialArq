3c41028fe2cc57886c0941a635d09f0ad0b11e79
Jorge Ortega Hernandez <jorge.ortegah@axity.com>
2021-02-05 10:03:53 -0600

    Changes in ulrs for deployment

--- front/src/Services/WebService.js
+++ front/src/Services/WebService.js
@@ -3,10 +3,10 @@ import {http} from '../Util/scripts/http.service';
 const TOKEN = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJndWlsbGVybW8uc2VndXJhQGF4aXR5LmNvbSIsImF1ZCI6IkludGVyZmF6Ok1lZ2EvUmVtZWR5Iiwic2NwIjoiYXBpL3BlcnNpc3RlbmNlIiwiaXNzIjoiaHR0cHM6Ly93d3cucHJvc2FtZXhpY28ubXgvIiwiZXhwIjo2MTYwNzkzNjI2MSwiaWF0IjoxNjA3OTM2MzIxfQ.J_gAk5pwbXjpm6aLF-mBIes66vKvpLIBWMZsoFxo6v4';
 
 // const urlBase = 'http://localhost:';
-//const urlBaseLogin = "http://web-myproject.192.168.99.100.nip.io";
-const urlBaseLogin = "http://web-prosa.192.168.99.103.nip.io";
-// const urlBasePersistence = "http://employee-myproject.192.168.99.100.nip.io";
-const urlBasePersistence = "http://employee-prosa.192.168.99.103.nip.io";
+const urlBaseLogin = "http://loginbk-persistencia.192.168.99.100.nip.io";
+// const urlBaseLogin = "http://web-prosa.192.168.99.103.nip.io";
+const urlBasePersistence = "http://persistenciacmdb-persistencia.192.168.99.100.nip.io";
+// const urlBasePersistence = "http://employee-prosa.192.168.99.103.nip.io";
 const portPersistence = '';
 const portWeb = '';
 const baseURL = '/v1';
