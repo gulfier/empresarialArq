7d4975a84fe15bc7635b4b8340331360b0d78cda
Jorge Ortega Hernandez <jorge.ortegah@axity.com>
2021-02-05 18:56:58 -0600

    Cambios url services

--- front/src/Components/Login/LoginComponent.js
+++ front/src/Components/Login/LoginComponent.js
@@ -26,7 +26,7 @@ function LoginComponent(props) {
   function login(){
     console.log("Se envio peticion con: ",dataLogin.user,dataLogin.password);
     props.getToken(dataLogin.user,dataLogin.password);
-    window.localStorage.setItem("token",res.data.response.token);
+    window.localStorage.setItem("token","Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJndWlsbGVybW8uc2VndXJhQGF4aXR5LmNvbSIsImF1ZCI6IkludGVyZmF6Ok1lZ2EvUmVtZWR5Iiwic2NwIjoiYXBpL3BlcnNpc3RlbmNlIiwiaXNzIjoiaHR0cHM6Ly93d3cucHJvc2FtZXhpY28ubXgvIiwiZXhwIjo2MTYwNzkzNjI2MSwiaWF0IjoxNjA3OTM2MzIxfQ.J_gAk5pwbXjpm6aLF-mBIes66vKvpLIBWMZsoFxo6v4");
     window.localStorage.setItem("login",true);
   }
 
