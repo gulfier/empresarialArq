a4b0809e8e8887c7e70150c372c0ec80e57f3caf
Jorge Ortega Hernandez <jorge.ortegah@axity.com>
2021-02-08 17:53:35 -0600

    Environment variables implemented

--- front/src/Services/WebService.js
+++ front/src/Services/WebService.js
@@ -2,14 +2,17 @@ import {http} from '../Util/scripts/http.service';
 
 // const urlBase = 'http://localhost:';
 // const urlBaseLogin = "http://loginbk-persistencia.192.168.99.100.nip.io";
-const urlBaseLogin = "http://web-prosa.192.168.99.104.nip.io";
+// const urlBaseLogin = "http://web-prosa.192.168.99.104.nip.io";
+const urlBaseLogin = process.env.URL_BASE_LOGIN;
 // const urlBasePersistence = "http://persistenciacmdb-persistencia.192.168.99.100.nip.io";
-const urlBasePersistence = "http://employee-prosa.192.168.99.104.nip.io";
+// const urlBasePersistence = "http://employee-prosa.192.168.99.104.nip.io";
+const urlBasePersistence = process.env.URL_BASE_PERSISTENCE;
 const portPersistence = '';
 const portWeb = '';
 const baseURL = '/v1';
 
 export function getDataConsola(token){
+    console.log("url:",urlBasePersistence);
     return http.get(urlBasePersistence+baseURL+'/CambiosAutorizar/',{
         headers: {
             Authorization: token
@@ -18,6 +21,7 @@ export function getDataConsola(token){
 }
 
 export function getDataLogin(user, password){
+    console.log("url login:",urlBaseLogin);
     var body = {
         "userName": user,
         "password": password,
